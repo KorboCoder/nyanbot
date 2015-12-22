2e2f4081f550f45cc0873c58efde6384f54eaad1
Daniel Vallesteros <daniel.v@pocketplaylab.com>
2015-12-23 01:01:27 +0800

    update to bash scripts

--- bin/hubot_facebook.sh
+++ bin/hubot_facebook.sh
@@ -2,4 +2,6 @@
 
  export FB_LOGIN_EMAIL="nyanbot805@gmail.com"
  export FB_LOGIN_PASSWORD="catgirlsareawesome"
+ export HUBOT_GOOGLE_CSE_ID="007060140865564271854:qzjscdzqwvm"
+ export HUBOT_GOOGLE_CSE_KEY="AIzaSyA6fyjhrSD7YB7mclDytlfeZ-fTNcRONeI"
  ./bin/hubot -a facebook
--- bin/hubot_messenger.sh
+++ bin/hubot_messenger.sh
@@ -2,4 +2,6 @@
 
  export HUBOT_FB_USERNAME="nyanbot805@gmail.com"
  export HUBOT_FB_PASSWORD="catgirlsareawesome"
+ export HUBOT_GOOGLE_CSE_ID="007060140865564271854:qzjscdzqwvm"
+ export HUBOT_GOOGLE_CSE_KEY="AIzaSyA6fyjhrSD7YB7mclDytlfeZ-fTNcRONeI"
  ./bin/hubot -a messenger
